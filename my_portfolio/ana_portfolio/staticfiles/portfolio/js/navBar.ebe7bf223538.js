3f5b8eadbacf205cd9950848b49c491af1d7de57
Ana Lemos <carol1692@hotmail.com>
2025-01-09 11:31:23 -0300

    update collectstatic

---
+++ my_portfolio/ana_portfolio/staticfiles/portfolio/js/navBar.ebe7bf223538.js
@@ -0,0 +1,10 @@
+function activeURL(element){
+    console.log(element)
+    // document.getElementById(element).classList.add("active","fw-bold")
+}
+
+
+function alertTeste(){
+    console.log("TESTE")
+    // document.getElementById(element).classList.add("active","fw-bold")
+}
