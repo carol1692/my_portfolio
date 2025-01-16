ee5f3bcf5c867f5ef05b3a459fbce9dc3f28a9fa
Ana Lemos <carol1692@hotmail.com>
2025-01-16 17:24:52 -0300

    remove sensitive information used on deploy tests

--- my_portfolio/ana_portfolio/ana_portfolio/settings.py
+++ my_portfolio/ana_portfolio/ana_portfolio/settings.py
@@ -54,14 +54,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
-SECRET_KEY = "haoshdohasdl,ashdjhalfkakjfkh" 
+SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
+# SECRET_KEY = "haoshdohasdl,ashdjhalfkakjfkh" 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
