0d21cb4ac07f3db816f7cb1ea5156c079d64c330
Ana Lemos <carol1692@hotmail.com>
2025-01-16 20:48:02 -0300

    Delete my_portfolio/ana_portfolio/staticfiles directory
    Delete staticfiles to reduce repository redundancy

--- my_portfolio/ana_portfolio/staticfiles/portfolio/js/cv.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#printer_PT-BR').on('click', function () {
-      $('#portuguese_content').printThis();
-    });
-    $('#printer_EN').on('click', function () {
-        $('#english_content').textContent.printThis()
-      });
-  });
