68090bfebb6ab25255a0607de9b29c4536f1215d
Ana Lemos <carol1692@hotmail.com>
2025-12-10 18:55:33 -0300

    deploy vercel fix allowed hosts

--- ana_portfolio/ana_portfolio/settings.py
+++ ana_portfolio/ana_portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 #SPOTIFY CREDENTIALS
 SPOTIFY_CLIENT_ID = os.getenv("CLIENT_ID")
